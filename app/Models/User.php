5156beb96d20f763c6ee162a3d2dddc217f2b08e
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2025-05-05 10:36:49 +0800

    diubah lagi
    Signed-off-by: Rifqah.S <146633614+Rifqah09@users.noreply.github.com>

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,11 +1,14 @@
 <?php
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
+class User extends Authenticatable
 {
+    use Notifiable;
     use HasFactory;
 
     protected $fillable = [
