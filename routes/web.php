d4a24518fded3422ee0d7813cbc70136c591afab
Rifqah.S <146633614+Rifqah09@users.noreply.github.com>
2025-05-26 09:42:37 +0800

    berubah
    Signed-off-by: Rifqah.S <146633614+Rifqah09@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\AlatCampingController;
-use App\Http\Controllers\PenyewaanController;
-use App\Http\Controllers\PembayaranController;
+
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,14 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::middleware(['auth', 'can:isAdminOrStaff'])->group(function () {
-    Route::resource('/alat-camping', AlatCampingController::class);
-});
-Route::resource('alat_camping', AlatCampingController::class)->middleware('auth');
-Route::resource('penyewaan', PenyewaanController::class)->middleware('auth');
-
-
-Route::get('penyewaan/{penyewaan}/pembayaran/create', [PembayaranController::class, 'create'])->name('pembayaran.create')->middleware('auth');
-Route::post('penyewaan/{penyewaan}/pembayaran', [PembayaranController::class, 'store'])->name('pembayaran.store')->middleware('auth');
+ });
 require __DIR__.'/auth.php';
